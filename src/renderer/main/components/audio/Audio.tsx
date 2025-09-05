2421996b52a83fdfb86da2c2e86a0e5838bd157b
redhoodsu <redhoodsu@tencent.com>
2025-09-05 12:54:25 +0800

    chore: small changes

--- src/renderer/main/components/audio/Audio.tsx
+++ src/renderer/main/components/audio/Audio.tsx
@@ -1,3 +1,10 @@
+import Style from './Audio.module.scss'
+import { t } from '../../../../common/util'
+
 export default function Audio() {
-  return <div></div>
+  return (
+    <div className={Style.container}>
+      <div className={Style.noAudios}>{t('noAudios')}</div>
+    </div>
+  )
 }
--- src/renderer/main/components/editor/Sample.tsx
+++ src/renderer/main/components/editor/Sample.tsx
@@ -1,10 +1,20 @@
 import { observer } from 'mobx-react-lite'
 import LunaAudioPlayer from 'luna-audio-player/react'
 import Style from './Sample.module.scss'
+import LunaToolbar from 'luna-toolbar/react'
+import ToolbarIcon from 'share/renderer/components/ToolbarIcon'
+import { t } from '../../../../common/util'
 
 export default observer(function Sample() {
   return (
     <div className={Style.container}>
+      <LunaToolbar className={Style.toolbar}>
+        <ToolbarIcon
+          icon="open-file"
+          title={t('openAudio')}
+          onClick={() => {}}
+        />
+      </LunaToolbar>
       <LunaAudioPlayer url="file:///Users/surunzi/project/liriliri/index/resources/sample.wav" />
     </div>
   )
